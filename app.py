c24bafc941807546d9801ce17b25fb9e0696d716
CHunYenc <yuhnoesty0402@hotmail.com.tw>
2020-07-01 00:54:11 +0800

    connect mysql

--- app.py
+++ app.py
@@ -1,24 +1,31 @@
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 from security import authenticate, identity
+
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost:3306/db_ad'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'jose'
 api = Api(app)
 
+##################################
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
+from models import *  
+# <--- move this anywhere after db is instantiated
 
+migrate.init_app(app, db)
+###################################
 
 jwt = JWT(app, authenticate, identity)  # /auth
 
@@ -28,7 +35,6 @@ api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
+
 if __name__ == '__main__':
-    from db import db
-    db.init_app(app)
     app.run(port=5000, debug=True)
